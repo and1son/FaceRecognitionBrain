0b68ac74ae420b12fede1ff2a665d91e834ef8ce
Andi Bašić <andi.basic10@gmail.com>
2018-10-01 21:50:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './components/navigation/Navigation'; 
+import Logo from './components/logo/logo'; 
+import ImageLinkForm from './components/imagelinkform/ImageLinkForm'; 
+import Rank from './components/rank/Rank'; 
+import './App.css';
+
+const particlesOptions = {
+    particles: {
+      number: {
+        value: 30,
+        density:{
+          enable:true,
+          value_area:800
+        }
+      }
+    }
+ }
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+       <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Navigation />
+          <Logo />
+          <Rank />
+          <ImageLinkForm />
+        { 
+          //<FaceRecognition />//
+        }
+      </div> 
+    );
+  }
+}
+
+export default App;
