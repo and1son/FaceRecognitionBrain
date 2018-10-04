f11b5adf438127e0f3d805876883118434c3e493
Andi Bašić <andi.basic10@gmail.com>
2018-10-04 20:04:35 +0200

    izmjene

--- src/App.js
+++ src/App.js
@@ -89,9 +89,18 @@ class App extends Component {
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL, 
       this.state.input)
-    .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
-        //console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-        // do something with response
+    .then(response => {
+      if(response){
+        fetch('http://localhost:3000/image',{
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            id: this.state.id
+        })
+      })
+    }
+      this.displayFaceBox(this.calculateFaceLocation(response))
+    })
     .catch(err => console.log(err));
   }
 
@@ -115,7 +124,10 @@ class App extends Component {
         { route === 'home' 
           ? <div>
               <Logo />
-              <Rank />
+              <Rank 
+                name={this.state.user.name}
+                entries={this.state.user.entries}
+              />
               <ImageLinkForm 
               onInputChange={this.onInputChange} 
               onButtonSubmit={this.onButtonSubmit} 
@@ -124,7 +136,7 @@ class App extends Component {
             </div> 
           : (
             route === 'signin'
-            ?  <SignIn onRouteChange={this.onRouteChange} />
+            ?  <SignIn loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
             :  <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
             )          
       }
